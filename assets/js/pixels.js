e6da98b181f5224f78243ffce6ab576e86668a51
autently <matteus.grodzki@autently.com>
2023-01-16 12:46:39 +0100

    Optimized and rearranged some code. The fading effect using the interval should work better now.

--- assets/js/pixels.js
+++ assets/js/pixels.js
@@ -5,28 +5,30 @@ document.addEventListener("DOMContentLoaded", function(){
     container.setAttribute("class", "pixel-grid");
     document.body.appendChild(container);
 
+    // Append 200 pixeltiles to the document
+    for(let i = 0; i < 200; i++){
+      tile();
+    }
+
+    // Change the color of all tiles with an interval
+    setInterval(function(){
+      const tiles = document.querySelectorAll(".pixelTile");
+      for(let j = 0; j < tiles.length; j++){
+        tiles[j].style.background = colorShades[getRandomIntInclusive()];
+      }
+    }, 1500);
+
+    // Create a tile
     function tile(){
-        
       let tile = document.createElement("div");
       tile.setAttribute('class', "pixelTile");
-        
       document.querySelector(".pixel-grid").appendChild(tile);
-      
-      setInterval(function(){
-            tile.style.background = colorShades[getRandomIntInclusive()];
-      }, 1500);
-      
     }
     
     function getRandomIntInclusive() {
       min = Math.ceil(0);
       max = Math.floor(colorShades.length);
       return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-    }
-    
-    
-    for(let i = 0; i < 200; i++){
-        tile();
     }
 
 });
